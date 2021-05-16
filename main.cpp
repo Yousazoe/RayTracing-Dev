0104d285df3d85a8c4bf2abda34db68cb5e50094
Yousazoe <961652168@qq.com>
2021-05-16 09:35:16 +0800

    adding a sphere

--- main.cpp
+++ main.cpp
@@ -2,12 +2,28 @@
 
 #include <iostream>
 
+bool hit_sphere(const vec3& center,double radius,const ray& r) {
+    vec3 oc = r.origin() - center;
+    auto a = dot(r.direction(),r.direction());
+    auto b = 2.0 * dot(oc,r.direction());
+    auto c = dot(oc,oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    return (discriminant > 0);
+}
+
 vec3 ray_color(const ray& r) {
+
+    if (hit_sphere(vec3(0,0,-1),0.5,r))
+        return vec3(1,0,0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
 
     return (1.0 - t) * vec3(1.0,1.0,1.0) + t * vec3(0.5,0.7,1.0);
 }
+
+
 
 int main() {
     const int image_width = 200;
